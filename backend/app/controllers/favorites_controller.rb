fc16cea6289e27514ef0af7db7a1f551f3485dca
mkonig411blk <margot.konig@blackrock.com>
2019-09-16 19:41:00 -0500

    added eventlistener for favoriting

--- backend/app/controllers/favorites_controller.rb
+++ backend/app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+    def new
+      favorite = Favorite.new
+    end
+
+    def create
+        favorite = Favorite.new(favorite_params)
+        render json: favorite, except: [:created_at, :updated_at]
+    end
+
+private
+    def favorite_params
+      params.require(:favorite).permit(:user_id, :gift_id)
+    end
 end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
+// JS file was required at bottom of HTML file so no need for DOMContentLoaded
 const BASE_URL = "http://localhost:3000"
 const GIFTS_URL = `${BASE_URL}/gifts`
 const USERS_URL = `${BASE_URL}/users`
+const FAVORITES_URL = `${BASE_URL}/favorites`
 const giftCollection = document.querySelector('#gift-collection')
 const likeButton = document.querySelector('.like-btn')
 const signupForm = document.querySelector('#signup-form')
 const signupInputs = document.querySelectorAll(".signup-input")
 let currentUser
 
-// JS file was required at bottom of HTML file so no need for DOMContentLoaded
+
 function putGiftsOnDom(giftArray){
     giftCollection.innerHTML = "<h2>All Gift Ideas</h2>"
     giftArray.forEach(gift => {
@@ -25,6 +27,10 @@ function fetchGifts(){
     fetch(GIFTS_URL)
     .then(res => res.json())
     .then(gifts => putGiftsOnDom(gifts))
+}
+
+function fetchFavorites(){
+
 }
 
 signupForm.addEventListener('submit', function(e){
@@ -57,8 +63,38 @@ signupForm.addEventListener('submit', function(e){
 function loggedInUser(object){
     currentUser = object
     signupForm.style.display = 'none'
-    welcome.innerText = `<h3>Welcome back, ${currentUser.name}!</h3>`
+    welcome.innerText = `Welcome back, ${currentUser.name}!`
     fetchGifts()
+    fetchFavorites()
 }
+
+
+giftCollection.addEventListener('click', function(e){
+    e.preventDefault()
+    if ((event.target.className == "like-btn") && (event.target.style.color !== '#FF0000')) {
+    fetch(FAVORITES_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            favorite: {
+                user_id: `${currentUser.id}`,
+                gift_id: `${event.target.dataset.giftId}`
+            }
+        })
+    })
+    .then(res => res.json())
+    .then(function(object){
+        console.log(object)
+    })
+    event.target.style.color = '#FF0000';}
+    // else if ((event.target.className == "like-btn") && (event.target.style.color == '#FF0000')) {
+    //     event.target.style.color = '#000000';
+    // }
+})
+
+
 
 fetchGifts()
